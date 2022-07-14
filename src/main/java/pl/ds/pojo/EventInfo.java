64a4737afc2adcc866c0fc2b569fca084dff2746
DanielSzcze <daniel.szcz3sny@gmail.com>
2022-07-15 00:56:35 +0200

    Added ScheduledProgram and EventInfo class

--- src/main/java/pl/ds/pojo/EventInfo.java
+++ src/main/java/pl/ds/pojo/EventInfo.java
@@ -1,4 +1,38 @@
 package pl.ds.pojo;
 
+import javax.xml.bind.annotation.XmlAttribute;
+
 public class EventInfo {
+
+    private String eventTrigger;
+    private String eventTag;
+
+    public EventInfo() {
+    }
+
+    @XmlAttribute(name = "EventTrigger")
+    public String getEventTrigger() {
+        return eventTrigger;
+    }
+
+    public void setEventTrigger(String eventTrigger) {
+        this.eventTrigger = eventTrigger;
+    }
+
+    @XmlAttribute(name = "EventTag")
+    public String getEventTag() {
+        return eventTag;
+    }
+
+    public void setEventTag(String eventTag) {
+        this.eventTag = eventTag;
+    }
+
+    @Override
+    public String toString() {
+        return "EventInfo{" +
+                "eventTrigger='" + eventTrigger + '\'' +
+                ", eventTag='" + eventTag + '\'' +
+                '}';
+    }
 }
--- src/main/java/pl/ds/pojo/ScheduledProgram.java
+++ src/main/java/pl/ds/pojo/ScheduledProgram.java
@@ -1,4 +1,25 @@
 package pl.ds.pojo;
 
+import javax.xml.bind.annotation.XmlAttribute;
+
 public class ScheduledProgram {
+
+    private String name;
+
+    public ScheduledProgram() {
+    }
+
+    @XmlAttribute(name = "Name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + "'";
+    }
 }
